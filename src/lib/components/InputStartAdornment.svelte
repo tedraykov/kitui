4810922356274f25e25b7a94aa751070a643fc63
tedraykov <tedraykov@gmail.com>
2022-08-21 12:51:13 +0300

    bug: input adornment doesn't need fragment wrapper

--- src/lib/components/InputStartAdornment.svelte
+++ src/lib/components/InputStartAdornment.svelte
@@ -1,7 +1,7 @@
-<script lang="ts">
+<script lang='ts'>
 	import { twMerge } from 'tailwind-merge';
 	import getThemeContext from '../styles/getThemeContext';
-	import { InputStartAdornmentProps } from '../types';
+	import type { InputStartAdornmentProps } from '../types';
 
 	const theme = getThemeContext();
 	const { defaultProps, styleOverrides } = theme.components.InputStartAdornment;
@@ -11,8 +11,6 @@
 	export let element: InputStartAdornmentProps['element'] = defaultProps.element;
 </script>
 
-<svelte:fragment>
-	<svelte:element this={element} class={twMerge('w-2', styleOverrides.root, _class)}>
-		<slot />
-	</svelte:element>
-</svelte:fragment>
+<svelte:element this={element} class={twMerge('w-2', styleOverrides.root, _class)}>
+	<slot />
+</svelte:element>
